33a4878f4c8314ca0e5c8fce060d5371a83bbbf4
ddpalacios <palaciosdanieldario@gmail.com>
2020-01-01 16:35:04 -0600

    At this point, I just added the size of our observations of our agent (4) and the size of the actions (How many actions? --> 2)

--- enviroments/CartPole_session/keras_cartpole_main.py
+++ enviroments/CartPole_session/keras_cartpole_main.py
@@ -1,5 +1,4 @@
 from collections import namedtuple
-
 import gym
 
 # Lets start by opening our environment
@@ -10,17 +9,23 @@ EpisodeStep = namedtuple('EpisodeStep', field_names=['observation', 'action'])
 
 if __name__ == '__main__':
     steps = 0
+    episode_reward = 0.0
     env = gym.make('CartPole-v0')
     observation = env.reset()  # Starting observation
+    observation_size = env.observation_space.shape[0]  # Amount of observations being tracked
+    action_size = env.action_space.n # Amount of potential actions. In our case --> [1,0]
 
     while True:
-        env.render()
+        # env.render()
         action = env.action_space.sample()
         obs, reward, done, _ = env.step(action)  # Observation from action
-        steps +=1
+        steps += 1
+        episode_reward += reward
+
         reward_status = Episode(reward=reward, steps=steps)
         status = EpisodeStep(observation=obs, action=action)
-        print("{}{}\n".format(status, reward_status))
+        # print("{}{}\n".format(status, reward_status))
         if done:
+            print("Earned {} rewards".format(episode_reward))
             env.close()
             break
