e569e35de0fdf6c1ebff88cca3c0637cc6f381c0
ddpalacios <45171403+ddpalacios@users.noreply.github.com>
2020-01-14 13:50:12 -0600

    Update main.py

--- MY_ENV/main.py
+++ MY_ENV/main.py
@@ -2,9 +2,7 @@ from Agent import Blob
 from Environment import Environment
 import random
 import sys
-from time import sleep
 if __name__ == '__main__':
-    print(sys.argv)
     test_env = Environment(window_size=int(sys.argv[1]), step_size=int(sys.argv[2]), world_size=int(sys.argv[3]))
     show = int(sys.argv[4])
     state = test_env.reset()
