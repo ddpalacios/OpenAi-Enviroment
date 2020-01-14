3ffadb484a7f42cfe2474db2c63f0d6a91c0fd93
ddpalacios <45171403+ddpalacios@users.noreply.github.com>
2020-01-14 13:48:12 -0600

    Add files via upload

--- MY_ENV/main.py
+++ MY_ENV/main.py
@@ -1,16 +1,21 @@
 from Agent import Blob
 from Environment import Environment
 import random
+import sys
 from time import sleep
 if __name__ == '__main__':
-    test_env = Environment()
+    print(sys.argv)
+    test_env = Environment(window_size=int(sys.argv[1]), step_size=int(sys.argv[2]), world_size=int(sys.argv[3]))
+    show = int(sys.argv[4])
     state = test_env.reset()
     while True:
         action = test_env.action_sample()
-        test_env.render()
+        if show:
+            test_env.render()
         state, reward, is_done = test_env.step(action)
-        print(action)
-    
+        if is_done:
+            break
+       
 
     
 
