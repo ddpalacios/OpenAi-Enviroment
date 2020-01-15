480fbb0e38af81cede898288753c3be222ffdf1a
Daniel <palaciosdanieldario@gmail.com>
2020-01-14 19:45:04 -0600

    debugger

--- MY_ENV/main.py
+++ MY_ENV/main.py
@@ -1,5 +1,5 @@
 
-
+from Environment import Environment
 if __name__ == '__main__':
     test_env = Environment(window_size=int(sys.argv[1]), step_size=int(sys.argv[2]), world_size=int(sys.argv[3]))
     show = int(sys.argv[4])
