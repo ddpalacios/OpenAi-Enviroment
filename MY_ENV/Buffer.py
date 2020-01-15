d1160af87d909c2988b6487b69dff825c4beb1b5
ddpalacios <palaciosdanieldario@gmail.com>
2020-01-15 14:36:53 -0600

    debugger

--- MY_ENV/Buffer.py
+++ MY_ENV/Buffer.py
@@ -1,5 +1,7 @@
 import collections
 import numpy as np
+from numpy import float32
+
 
 class ExperienceBuffer:
     def __init__(self, capacity):
@@ -13,7 +15,7 @@ class ExperienceBuffer:
 
     def sample(self, batch_size):
         indices = np.random.choice(len(self.buffer), batch_size, replace=False)
-        states, actions. rewards, dones, next_states = zip(*[self.buffer[idx] for idx in indices])
+        states, actions, rewards, dones, next_states = zip(*[self.buffer[idx] for idx in indices])
         return np.array(states), np.array(actions), np.array(rewards, dtype=float32), \
                np.array(dones, dtype=np.uint8), \
                np.array(next_states)
